3e1ae17014e6f6de8060659c4745fa11d878590e
itssolomonT <itssolomontufail@gmail.com>
2026-04-22 14:38:18 -0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          I am a results-driven software engineer specializing in React and React Native, crafting high-performance web and mobile applications.
+I am a results-driven software engineer specializing in React and React Native, crafting high-performance web and mobile applications.
 
         </p>
       </div>
