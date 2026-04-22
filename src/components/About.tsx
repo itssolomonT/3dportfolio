e4b39a57b6ebed89f25d5dd2bc9ac26eb0476859
itssolomonT <itssolomontufail@gmail.com>
2026-04-22 14:38:43 -0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,6 +8,10 @@ const About = () => {
         <p className="para">
 I am a results-driven software engineer specializing in React and React Native, crafting high-performance web and mobile applications.
 
+          With 5 years of experience as a web developer, I have honed my ability to understand client needs and industry trends. I’ve worked with businesses across various niches, delivering modern, user-friendly, and high-performing web solutions.
+
+With 5 years of experience, I’ve worked across diverse design areas, including front-end development, landing pages, email design, app UI/UX, and mobile platform product design. Having contributed to live projects, I offer a range of options and ideas for us to collaborate on, ensuring you’re not limited to a single direction.
+
         </p>
       </div>
     </div>
