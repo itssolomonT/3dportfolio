41d632827878e6c65f329172a50a486467891041
itssolomonT <itssolomontufail@gmail.com>
2026-04-22 15:06:03 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          AM
+          ST
         </a>
         <a
           href="https://github.com/itssolomonT"
